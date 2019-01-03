5f0c134dcd40d60f1737b6d902214915a2c15a37
Barak A. Pearlmutter <bap@debian.org>
2019-01-03 19:28:54 +0100

    Import Upstream version 3.4

--- danlib.c
+++ danlib.c
@@ -1,7 +1,7 @@
 /*
-   $Id: danlib.c,v 1.2 1998/03/27 17:58:20 danny Exp $
+   $Id: danlib.c,v 1.6 2004/02/25 04:49:35 sadinoff Exp $
    Hebcal - A Jewish Calendar Generator
-   Copyright (C) 1994  Danny Sadinoff
+   Copyright (C) 1994-2004  Danny Sadinoff
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -18,50 +18,41 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
    Danny Sadinoff can be reached at 
-   1 Cove La.
-   Great Neck, NY
-   11024
-
-   sadinoff@pobox.com 
+   danny@sadinoff.com
  */
 
 #include <stdio.h>
 #include <sys/types.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 #include "danlib.h"
 #include "myerror.h"
 
 
 /* Some generally useful routines */
 
-void 
-initStr (s, size)
-     char **s;
-     int size;
+void initStr( char **s, size_t size )
 {
-  /* allocate space for a string */
-  if ((*s = (char *) malloc ((size_t) (size + 1) * sizeof (char))) == NULL)
-      die ("\n Memory Error: Couldn't allocate string", "");
-  **s = '\0';
+    /* allocate space for a string */
+    if ((*s = (char *) malloc ((size + 1) * sizeof (char))) == NULL)
+        die ("\n Memory Error: Couldn't allocate string", "");
+    **s = '\0';
 }
 
 /* istrncasecmp performs a signed, case-insensitive comparison
    of s1 to s2, for a maximum length of n  bytes. */
-int 
-istrncasecmp (n, s1, s2)
-     char *s1, *s2;
-     int n;
+int istrncasecmp( size_t n, const char *s1, const char *s2 )
 {
-  for (;
-       n > 0 &&			/* while not at end of string */
-       (*s1 || *s2);		/* and they both didn't finish simultaneously */
-       n--, s1++, s2++)
-    if (tolower (*s1) != tolower (*s2))
-      {
-	return tolower (*s1) < tolower (*s2) ? -1 : 1;
-      }
-  return 0;
+    for (;
+         n > 0 &&			/* while not at end of string */
+             (*s1 || *s2);		/* and they both didn't finish simultaneously */
+         n--, s1++, s2++)
+        if (tolower (*s1) != tolower (*s2))
+        {
+            return tolower (*s1) < tolower (*s2) ? -1 : 1;
+        }
+    return 0;
 }
 
 /* find the first element of arr which is a superstring of str, to len
@@ -69,62 +60,54 @@ istrncasecmp (n, s1, s2)
    Return the index of the element.  If there is no such element, 
    Return size.  size is the size of arr. 
  */
-int 
-lookup_string (str, arr, size, len)
-     char *str;
-     char *arr[];
-     int size;
-     int len;
+int lookup_string (const char *str, const char *arr[], int size, int len)
 {
-  int i;
-
-  for (i = 0; i < size; i++)
-    if (!(istrncasecmp (len, str, arr[i])))
-      return i;
-  return i;			/* i == size here. */
+    int i;
+    
+    for (i = 0; i < size; i++)
+        if (!(istrncasecmp (len, str, arr[i])))
+            return i;
+    return i;			/* i == size here. */
 }
 
 
 
-int 
-isAllNums (s)
-     char *s;
+int isAllNums ( const char *s )
 {
 
 /* returns true if a string contains only digits */
-  int n = 0, len = strlen (s);
+  size_t n = 0;
+  size_t len = strlen( s );
 
   for (n = 0;
-       (n < len) && isdigit (s[n]);
+       (n < len) && isdigit( (int) s[n] );
        n++);
   return (n == len);
 }
 
-char *
-numSuffix (i)
+
 /* returns he proper ordinal suffix of a number */
-     int i;
+const char *
+    numSuffix (int i)
 {
-  if ((i / 10) == 1)
-    return "th";
-  switch (i % 10)
+    if ((i / 10) == 1)
+        return "th";
+    switch (i % 10)
     {
     case 1:
-      return "st";
+        return "st";
     case 2:
-      return "nd";
+        return "nd";
     case 3:
-      return "rd";
+        return "rd";
     default:
-      return "th";
+        return "th";
     }
 }
 
-char *
-hc_itoa (i)
-     int i;
+char * hc_itoa (int i)
 {
-  static char ret[7];
-  sprintf (ret, "%d", i);
-  return ret;
+    static char ret[7];         /* FIX: eeeviiill */
+    sprintf (ret, "%d", i);
+    return ret;
 }
